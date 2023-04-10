0c8f45aa84b142e80bfd7f472eb60898069a2f2e
Rahad <rahadarafin3@gmail.com>
2023-04-11 01:47:36 +0600

    B.Even

--- B. Even Numbers.c
+++ B. Even Numbers.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
 int main(){
-
-int i,N;
-scanf("%d",&N);
-if(N==1)
+int i,n,x,even=0,odd=0,negative=0,positive=0;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
 {
-    printf("1\n");
-}
-else
-
-{
-
-    for(i=1;i<=N;i++)
+    scanf("%d",&x);
+    if(x%2==0)
     {
-
-
-        if(i%2==0)
-        {
-            printf("%d\n",i);
+        even++;
         }
-    }
+   else
+   {
+       odd++;
+   }
+    if(x>0)
+   {
+     positive++;
+   }
+   else if(x<0)
+   {
+       negative++;
+   }
 }
-
-
-
+printf("Even=%d\n",even);
+printf("Odd=%d\n",odd);
+printf("Positive=%d\n",positive);
+printf("Negative=%d\n",negative);
 
 return 0;}
